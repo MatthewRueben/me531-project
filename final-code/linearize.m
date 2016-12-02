8dbce924386f26db5653405b659581ab01765a01
Numizi <nguyend6@onid.oregonstate.edu>
2016-12-01 23:50:51 -0800

    some final clean up

--- final-code/linearize.m
+++ final-code/linearize.m
@@ -11,14 +11,15 @@
 %f = m2 * d2dd - m2*d2*(v1d)^2 + m2*g*sin(v1);
 %var = [d2, d2d, d2dd, v1, v1d, v1dd];
 %pivot = [3, 0, 0, 0, 0, 0];
+%lin = get_linearized_function(f,var,pivot)
 
 function eqL = get_linearized_function(eqNL,var,pivot)
-    jab = jacobian(eqNL,var);
-    f0 = subs(eqNL,var,pivot); 
-    for i = 1:6
-        f0 = f0 + subs(jab(i),var,pivot)*(var(i) - pivot(i));
+    jab = jacobian(eqNL,var); %get the partial direvative 
+    f0 = subs(eqNL,var,pivot); %the first part of the Taylor series 
+    [col,row] = size (var); %get the number of variable in the function
+    for i = 1:row
+        f0 = f0 + subs(jab(i),var,pivot)*(var(i) - pivot(i)); %add the partial direvative
     end
-
     eqL = f0;
 end
 
