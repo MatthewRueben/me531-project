3479606851ef819f7f1ae21f580740f87d939dd5
Matt Rueben <ruebenm@oregonstate.edu>
2016-12-01 21:05:24 -0800

    Using the Lagrange() function downloaded from the web instead of our own lagrangian() function, which I have deleted. The script test_lagrangian.m now gets the non-linear EoM and feed-forward force to keep it at the set point.

--- final-code/test_lagrangian.m
+++ final-code/test_lagrangian.m
@@ -4,12 +4,28 @@
 syms m k  % constants
 syms x dx ddx  % state variables
 syms F  % inputs
-state = [x dx]';
-state_deriv = [dx ddx]';
+
+state = [x dx];  % UNUSED
+state_deriv = [dx ddx];  % UNUSED
+state_vars = [x dx ddx];
+
 eqKE = 1/2 * m * dx^2;
 eqPE = 1/2 * k * x^2; 
 forces_or_torques = [F];
-set_point = [0.10   0.0];  % release from 10cm
-[FFT , eqNL] = lagrangian(eqKE, eqPE, state, state_deriv, forces_or_torques, set_point); 
-display(FFT)
-display(eqNL)
+
+set_point = [0.10   0.0   0.0];  % release from 10cm
+
+% Calculate Lagrangian
+L = eqPE - eqKE; % so eqn should be dL/dx - d/dt(dL/dx_dot) = F|tau
+constants = [m k];
+choices = [2.0 5.0];  % for the constants
+L = subs(L, constants, choices);
+
+% Do Lagrange equation
+addpath ./external-code  % so we can call Lagrange() from inside a folder
+eqns = Lagrange(L, state_vars);
+display(eqns)
+
+% Solve for feed-forward force or torque
+ffF = -1 * subs(eqns(1), state_vars, set_point);
+display(ffF)
